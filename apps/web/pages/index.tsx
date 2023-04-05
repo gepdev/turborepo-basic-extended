f0b9287df432ea71dbe2377b056a66b658b6f112
Gentian Polovina <gentian.polovina@capgemini.com>
2023-04-05 18:23:12 +0200

    chore: test og tags

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,15 @@
+import Head from "next/head";
 import { Button } from "ui";
 
 export default function Web() {
   return (
-    <div>
+    <>
+    <Head>
+      <meta name="og:title" content="Title di prova" />
+      <meta name="og:url" content="https://example.com" />
+    </Head>
       <h1>Web</h1>
       <Button />
-    </div>
+    </>
   );
 }
