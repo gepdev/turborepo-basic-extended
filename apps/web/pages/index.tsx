b62f6fa0b5532a9389f49a295bc8510566d31829
Gentian Polovina <gentian.polovina@capgemini.com>
2023-04-05 18:40:16 +0200

    chore: change of og url

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -5,9 +5,9 @@ export default function Web() {
   return (
     <>
     <Head>
-      <link rel="canonical" href="https://monorepo-design-system.vercel.app/" />
+      <link rel="canonical" href="https://master.d2nmw557tj47ph.amplifyapp.com/about.html" />
       <meta name="og:title" content="Title di prova" />
-      <meta name="og:url" content="https://monorepo-design-system.vercel.app/" />
+      <meta name="og:url" content="https://master.d2nmw557tj47ph.amplifyapp.com/about.html" />
     </Head>
       <h1>Web</h1>
       <Button />
