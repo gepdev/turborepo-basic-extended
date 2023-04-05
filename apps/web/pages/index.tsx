c1ef385f13035d8549d140c9186a666884920fde
Gentian Polovina <gentian.polovina@capgemini.com>
2023-04-05 18:33:16 +0200

    chore: test with og tags

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -5,8 +5,9 @@ export default function Web() {
   return (
     <>
     <Head>
+      <link rel="canonical" href="https://monorepo-design-system.vercel.app/" />
       <meta name="og:title" content="Title di prova" />
-      <meta name="og:url" content="https://example.com" />
+      <meta name="og:url" content="https://monorepo-design-system.vercel.app/" />
     </Head>
       <h1>Web</h1>
       <Button />
