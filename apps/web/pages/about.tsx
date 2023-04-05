022a7b845a23720870dd81e82ce9e51eda021e32
Gentian Polovina <gentian.polovina@capgemini.com>
2023-04-05 18:41:24 +0200

    chore: about page

---
+++ apps/web/pages/about.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import { Button } from "ui";
+
+export default function Web() {
+  return (
+    <>
+    <Head>
+      <meta name="og:title" content="About page" />
+      <meta name="og:url" content="https://master.d2nmw557tj47ph.amplifyapp.com/about.html" />
+    </Head>
+    <main>
+      <h1>About Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatem fuga veritatis tempora necessitatibus dolorum animi consectetur, molestiae consequuntur aliquid! Magnam vitae at ea laborum fuga excepturi reprehenderit consequuntur cupiditate.</p>
+    </main>
+    </>
+  );
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -5,7 +5,6 @@ export default function Web() {
   return (
     <>
     <Head>
-      <link rel="canonical" href="https://master.d2nmw557tj47ph.amplifyapp.com/about.html" />
       <meta name="og:title" content="Title di prova" />
       <meta name="og:url" content="https://master.d2nmw557tj47ph.amplifyapp.com/about.html" />
     </Head>
